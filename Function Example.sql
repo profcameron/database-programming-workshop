6fe070dc7c185bdec05ed31f9a82868432d35f32
Eric Cameron <ericcameronnj@gmail.com>
2023-12-14 16:57:09 -0500

    Update Function Example.sql
    Switch over to an airport example

--- Function Example.sql
+++ Function Example.sql
@@ -1,15 +1,13 @@
 -- Written against a PostgreSQL database 
--- Eric Cameron, Java Instructor, Tech Elevator, 04/16/2023
 -- Written to show a simple function for a bonus lecture
 
 -- Function accepts a parameter named arg of type
--- numeric size (5,1) - this matches the data type
--- of the area field in the UnitedStates database
--- we use at Tech Elevator
+-- int - this matches the data type
+-- of the elevation field in the Airport database
 
--- Returns a character of size 6 (Large, Normal, or Small)
-CREATE OR REPLACE FUNCTION func_display_size(arg numeric(5,1))
-RETURNS char(6)
+-- Returns a character of size 5 (Near, Above, or High)
+CREATE OR REPLACE FUNCTION func_display_elevation(arg int)
+RETURNS char(5)
 -- PostgreSQL requires a language to be set
 LANGUAGE plpgsql AS
 -- We use the $$ as a delimiter for start and end of our function
@@ -19,12 +17,12 @@ DECLARE
 
 BEGIN
   -- Function will return a value (functions return values, procedures do not)
-  IF arg > 10000 THEN
-    RETURN 'Large';
-  ELSIF arg > 1000 THEN
-    RETURN 'Normal';
+  IF arg > 5280 THEN
+    RETURN 'High';
+  ELSIF arg < 1000 THEN
+    RETURN 'Near';
   ELSE
-    RETURN 'Small';
+    RETURN 'Above';
   END IF;
 
 END;
@@ -32,6 +30,6 @@ $$ ;
 
 -- After creatng the function you can call it as part of a query
 -- Just make sure you match your data types.
-SELECT park_name, area, func_display_size(area) AS Size
-FROM park
-ORDER BY area DESC;
+SELECT name, elevation, func_display_elevation(elevation) AS elevation_desc
+FROM airport
+ORDER BY elevation DESC;
